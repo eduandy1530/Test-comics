a85deb42742d9585e55a172541e6324e60b99bfc
Eduandy <eicaabreu30@gmail.com>
2021-11-28 13:00:03 -0400

    init commit

---
+++ android/app/src/main/kotlin/com/example/comics_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.comics_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config/app_config.dart
@@ -0,0 +1,4 @@
+class AppConfig {
+  static const API_URL = "https://comicvine.gamespot.com/api/";
+  static const api_key = '86b409337c811dca0888a41f689197719a165d50';
+}
